e9043b19e806388e85ef1274e76ae7b5c2f56866
Shubham-Kelkar <mscds.2415@unigoa.ac.in>
2026-02-04 09:43:34 +0530

    fix: protected input and renamed test file

--- src/Student_Grade_Calculator.py
+++ src/Student_Grade_Calculator.py
@@ -2,24 +2,25 @@ def Calculate_grade(marks):
     if not marks:
         return "No marks provided"
     
-    average = sum(marks)/ len(marks)
+    average = sum(marks) / len(marks)
 
     if average >= 90:
-      return "A+"
+        return "A+"
     elif average >= 75:
-       return "A"
+        return "A"
     elif average >= 60:
-       return "B"
+        return "B"
     elif average >= 50:
-       return "C"
+        return "C"
     else:
-       return "Fail"
-    
-user_marks = []
+        return "Fail"
 
-for i in range(1, 6):
-    m = float(input(f"Enter marks for subject {i}: "))
-    user_marks.append(m)
+# THIS BLOCK PROTECTS THE TESTS FROM CRASHING
+if __name__ == "__main__":
+    user_marks = []
+    for i in range(1, 6):
+        m = float(input(f"Enter marks for subject {i}: "))
+        user_marks.append(m)
 
-result = Calculate_grade(user_marks)
-print("Final Grade:", result)
+    result = Calculate_grade(user_marks)
+    print("Final Grade:", result)
